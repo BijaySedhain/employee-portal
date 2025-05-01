494d038e48db9c2957992fd7ebb1716b85a20426
Bijay Sedhain <bijaysedhain@gmail.com>
2025-05-01 15:49:11 -0500

    Create a mapper

--- src/main/java/com/employees/mapper/EmployeeMapper.java
+++ src/main/java/com/employees/mapper/EmployeeMapper.java
@@ -3,56 +3,19 @@ package com.employees.mapper;
 
 import com.employees.dto.EmployeeDTO;
 import com.employees.entity.Employee;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-public class EmployeeMapper {
+@Mapper(componentModel = "spring",
+        uses = {LocalDateMapper.class})
+public interface EmployeeMapper {
 
-    /**
-     * Converts an Employee entity to an EmployeeDTO.
-     *
-     * @param employee the Employee entity to convert
-     * @return the converted EmployeeDTO
-     */
-    public EmployeeDTO toDTO(Employee employee) {
-        if (employee == null) {
-            return null;
-        }
-        EmployeeDTO employeeDTO = new EmployeeDTO();
-        employeeDTO.setId(employee.getId());
-        employeeDTO.setFirstName(employee.getFirstName());
-        employeeDTO.setLastName(employee.getLastName());
-        employeeDTO.setEmail(employee.getEmail());
-        employeeDTO.setPhone(employee.getPhone());
-        employeeDTO.setJobTitle(employee.getJobTitle());
-        employeeDTO.setDepartment(employee.getDepartment());
-        employeeDTO.setHireDate(employee.getHireDate());
-        employeeDTO.setEmploymentType(employee.getEmploymentType());
-        employeeDTO.setEmergencyContact(employee.getEmergencyContact());
+    EmployeeDTO toDto(Employee employee);
 
-        return employeeDTO;
-    }
+    Employee toDao(EmployeeDTO EmployeeDto);
 
-    /**
-     * Converts an EmployeeDTO to an Employee entity.
-     *
-     * @param employeeDTO the EmployeeDTO to convert
-     * @return the converted Employee entity
-     */
-    public Employee toEntity(EmployeeDTO employeeDTO) {
-        if (employeeDTO == null) {
-            return null;
-        }
-        Employee employee = new Employee();
-        employee.setId(employeeDTO.getId());
-        employee.setFirstName(employeeDTO.getFirstName());
-        employee.setLastName(employeeDTO.getLastName());
-        employee.setEmail(employeeDTO.getEmail());
-        employee.setPhone(employeeDTO.getPhone());
-        employee.setJobTitle(employeeDTO.getJobTitle());
-        employee.setDepartment(employeeDTO.getDepartment());
-        employee.setHireDate(employeeDTO.getHireDate());
-        employee.setEmploymentType(employeeDTO.getEmploymentType());
-        employee.setEmergencyContact(employeeDTO.getEmergencyContact());
+    @Mapping(target = "id", ignore = true)
+    void updateEmployeeFromDto(EmployeeDTO dto, @MappingTarget Employee employee);
 
-        return employee;
-    }
 }
