65961f045f9a0f4e23bccfd2fb0ed1aa95ec2482
Bijay Sedhain <bijaysedhain@gmail.com>
2025-05-07 13:25:59 -0500

    import optimization

--- src/main/java/com/employees/controller/EmployeeController.java
+++ src/main/java/com/employees/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.employees.controller;
 import com.employees.dto.EmployeeDTO;
 import com.employees.entity.Employee;
 import com.employees.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
     private final EmployeeService employeeService;
 
@@ -27,25 +31,33 @@ public class EmployeeController {
 
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable Long id) {
+        logger.info("Received request to get employee with id: {}", id);
         EmployeeDTO employeeDTO = employeeService.getEmployeeById(id);
+        logger.info("Successfully retrieved employee with id: {}", id);
         return new ResponseEntity<>(employeeDTO, HttpStatus.OK);
     }
 
     @PostMapping("/save")
     public ResponseEntity<EmployeeDTO> saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        logger.info("Received request to save employee: {}", employeeDTO);
         EmployeeDTO savedEmployee = employeeService.saveEmployee(employeeDTO);
+        logger.info("Successfully saved employee with ID: {}", savedEmployee.getId());
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<EmployeeDTO> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDTO employeeDTO) {
+        logger.info("Received request to update employee with id: {}", id);
         EmployeeDTO updatedEmployee = employeeService.updateEmployee(id, employeeDTO);
+        logger.info("Successfully updated employee with id: {}", id);
         return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        logger.info("Received request to delete employee with id: {}", id);
         employeeService.deleteEmployee(id);
+        logger.info("Successfully deleted employee with id: {}", id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
